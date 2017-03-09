a5bf9585ca4e088a93fd8f9d535e197bf4914216
leo <merlinkael@gmail.com>
2017-03-09 22:10:25 +0800

    上传一个 redis监听器的配置

---
+++ fruit-es/fruit-es-service/src/main/java/com/fruit/es/listener/RedisTestListener.java
@@ -0,0 +1,29 @@
+package com.fruit.es.listener;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.redis.connection.Message;
+import org.springframework.data.redis.listener.KeyspaceEventMessageListener;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+
+
+/**
+ * @author leo
+ * redis定时调度监听器
+ */
+public class RedisTestListener extends KeyspaceEventMessageListener{
+	
+	private static Logger logger=LoggerFactory.getLogger(RedisTestListener.class);
+
+	public RedisTestListener(RedisMessageListenerContainer listenerContainer) {
+		super(listenerContainer);
+		
+	}
+
+	@Override
+	protected void doHandleMessage(Message message) {
+		String key = new String(message.getBody());	
+		logger.info("redis key is {}...",key);
+	}
+
+}
